ea68fdaadf920fe67eea8954c6d2d54cb1889e0e
Aneesh-design <69197689+Aneesh-design@users.noreply.github.com>
2021-10-31 21:19:23 +0545

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,14 @@
+<script type = "text/javascript">
+  
+// Function definition
+function welcomeMsg(name) {
+   document.write("Hello " + name + " welcome to GeeksforGeeks");
+}
+  
+// creating a variable
+var nameVal = "Admin";
+  
+// calling the function
+welcomeMsg(nameVal);
+  
+</script>
